4db8cc761b1f1a98b7f1a4b485a4b66da85e452f
yossefsameh <yossefsameh23@gmail.com>
2022-09-01 18:01:46 +0200

    first

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ ! -d ./database ]
+then
+mkdir ./database
+fi
+. ./main_menu.sh
---
+++ main_menu.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+PS3="enter your choice: "
+select choice in "type 1 to creat database" "type 2 to list database" "type 3 to drop database" "type 4 to connect to database" 
+do 
+case $REPLY in 
+1) echo "enter new database name: "
+   read dbname
+   if [ ! -d ./database/"$dbname" ]
+   then
+   	case "$dbname" in
+   	+([[:upper:]])) mkdir ./database/"$dbname"
+   	;;
+   	+([[:lower:]])) mkdir ./database/"$dbname"
+   	;;
+   	+([0-9])) echo name of data base can"'"t be a number
+   	;;
+   	+([A-Za-z0-9])) mkdir ./database/"$dbname"
+   	;;
+   	*) echo wrong name
+   	;;
+   	esac
+   	#mkdir ./database/"$dbname"
+   else
+   echo "this name is taken" 
+   fi
+;;	
+2) ls -F ./database| grep / 
+;;
+3) echo "enter name of database you want to remove"
+   read rmname
+   if [ -d ./database/"$rmname" ]
+   then
+   rm -r ./database/"$rmname"
+   else
+   echo "this name is not found"
+   fi
+;;
+4) echo "enter name of database you want to connect"
+   read cname
+   if [ -d ./database/"$cname" ]
+   then
+   #cd ./database/"$cname"
+   . ./connect_menu.sh
+   else
+   echo "this name is not found"
+   fi
+
+;;
+*)break ;;
+esac
+done
